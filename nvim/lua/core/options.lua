2cbfd16e6acbfa351e3d8119924cb70e917264cb
Sewak Singh Gill <sewak@Sewaks-MacBook-Air.local>
2025-10-24 23:38:18 -0400

    show line numbers in open editors

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -3,3 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.ruler = true
