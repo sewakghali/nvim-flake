131f3e5c4978ef65817c4182eb0aeafe1465a143
Sewak Singh Gill <sewak@Sewaks-MacBook-Air.local>
2025-10-26 12:10:07 -0400

    added default langs to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,1 +1,33 @@
-return { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" }
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				lua,
+				javascript,
+				typescript,
+				c,
+				css,
+				html,
+				typescript,
+				svelte,
+				dart,
+				json,
+				jsonc,
+				java,
+				kotlin,
+				swift,
+				markdown,
+				markdown_inline,
+				nginx,
+				dockerfile,
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
