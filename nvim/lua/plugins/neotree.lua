dc7503fb7aa9af9edf476aa7781b1e8bc8e0f918
Sewak Singh Gill <sewak@Sewaks-MacBook-Air.local>
2025-10-25 20:39:24 -0400

    replaced ctrl b with leader b because ctrl b is mapped to zellij

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -9,7 +9,7 @@ return {
     },
     lazy = false,                 -- neo-tree will lazily load itself
     config = function()
-      vim.keymap.set("n", "<C-b>", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+      vim.keymap.set("n", "<leader>b", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
       require("neo-tree").setup({
         -- close_if_last_window = true,
         -- enable_git_status = false,
