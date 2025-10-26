061ff0fe9b61e50184fe39c1db29c7ee5f717838
Sewak Singh Gill <sewak@Sewaks-MacBook-Air.local>
2025-10-26 11:25:16 -0400

    use lsp-proxy subcommand on postgrestools to run postgres LSP server

--- nvim/lsp/postgrestools.lua
+++ nvim/lsp/postgrestools.lua
@@ -1,37 +1,37 @@
-local get_env = function(name)
-	return os.getenv(name) or nil
-end
+-- local get_env = function(name)
+--   return os.getenv(name) or nil
+-- end
 
 return {
-	cmd = { "postgrestools" },
-	filetypes = {
-		"sql",
-	},
+  cmd = { "postgrestools", "lsp-proxy" },
+  filetypes = {
+    "sql",
+  },
 
-	settings = {
-		["$schema"] = "https://github.com/supabase-community/postgres-language-server/blob/a621abcc66bfa300cbfb6d115b28d4027d629df0/docs/schema.json",
-		-- "vcs": {
-		--   "enabled": false,
-		--   "clientKind": "git",
-		--   "useIgnoreFile": false
-		-- },
-		-- "files": {
-		--   "ignore": []
-		-- },
-		linter = {
-			enabled = true,
-			rules = {
-				recommended = true,
-			},
-		},
-		db = {
-			host = get_env("PG_HOST"),
-			port = tonumber(get_env("PG_PORT") or "5432"),
-			username = get_env("PG_USER"),
-			password = get_env("PG_PASSWORD"),
-			database = get_env("PG_DB"),
-			connTimeoutSecs = 10,
-			allowStatementExecutionsAgainst = { "postgres/*", "localhost/*" },
-		},
-	},
+  -- settings = {
+  -- 	["$schema"] = "https://github.com/supabase-community/postgres-language-server/blob/a621abcc66bfa300cbfb6d115b28d4027d629df0/docs/schema.json",
+  -- 	-- "vcs": {
+  -- 	--   "enabled": false,
+  -- 	--   "clientKind": "git",
+  -- 	--   "useIgnoreFile": false
+  -- 	-- },
+  -- 	-- "files": {
+  -- 	--   "ignore": []
+  -- 	-- },
+  -- 	linter = {
+  -- 		enabled = true,
+  -- 		rules = {
+  -- 			recommended = true,
+  -- 		},
+  -- 	},
+  -- 	db = {
+  -- 		host = get_env("PG_HOST"),
+  -- 		port = tonumber(get_env("PG_PORT") or "5432"),
+  -- 		username = get_env("PG_USER"),
+  -- 		password = get_env("PG_PASSWORD"),
+  -- 		database = get_env("PG_DB"),
+  -- 		connTimeoutSecs = 10,
+  -- 		allowStatementExecutionsAgainst = { "postgres/*", "localhost/*" },
+  -- 	},
+  -- },
 }
