94681fe0e4b47acbf56b254ee3913b8766ebf8ac
Sewak Singh Gill <sewak@Sewaks-MacBook-Air.local>
2025-10-26 12:10:50 -0400

    improved git integration with telescope, git signs, and trouble for project diagnostics

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,5 +7,9 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>p", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
+    vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
+    vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+    vim.keymap.set("n", "<leader>gst", builtin.git_stash, {})
   end,
 }
