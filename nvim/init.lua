bd38bd8e3dcb99f318561710a66c60f46adbee90
Sewak Singh Gill <sewaksingh2212@gmail.com>
2026-02-22 02:08:47 -0500

    added path to make init.lua visible to the derived shells

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
+local current_file = debug.getinfo(1, "S").source:sub(2)
+local nvim_root = current_file:match("(.*)/init.lua")
+
+if nvim_root then
+    vim.opt.rtp:prepend(nvim_root)
+end
+
 require("core.customcommands")
 require("core.options")
 require("core.lazy")
